5a8050624cb051ba5df7c22e8caf1bec31abefc9
Danil Shabanov <shabanov1903@gmail.com>
2021-09-02 11:50:38 +0300

    Create WebApp1

---
+++ WebApp1/WebApp1/Controllers/TemperatureController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemperatureController : Controller
+    {
+        private readonly ValuesHolder _valuesHolder;
+
+        public TemperatureController(ValuesHolder valuesHolder) => _valuesHolder = valuesHolder;
+
+        [HttpPost]
+        public IActionResult IndexPost([FromBody] Values value)
+        {
+            _valuesHolder.Add(value);
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult IndexPut([FromBody] Values value)
+        {
+            _valuesHolder.Put(value);
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult IndexGet([FromQuery] DateTime StartTime, [FromQuery] DateTime FinishTime)
+        {
+            return Ok(_valuesHolder.Sort(StartTime, FinishTime));
+        }
+
+        [HttpDelete]
+        public IActionResult IndexDel([FromQuery] DateTime StartTime, [FromQuery] DateTime FinishTime)
+        {
+            _valuesHolder.Del(StartTime, FinishTime);
+            return Ok();
+        }
+    }
+}
---
+++ WebApp1/WebApp1/ValuesHolder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp1
+{
+    public class ValuesHolder
+    {
+        public List<Values> valuesHolderList = new List<Values>();
+
+        // Функция Add предназначения для вставки элемента
+        public void Add(Values value)
+        {
+            valuesHolderList.Add(value);
+        }
+
+        // Функция Put предназначения для изменения значения
+        // В соответствии с заданным временем
+        public void Put(Values value)
+        {
+            foreach (Values vs in valuesHolderList)
+            {
+                if (vs.Date.CompareTo(value.Date) == 0) vs.Temperature = value.Temperature;
+            }
+        }
+
+        // Функция Del предназначения для удаления элементов
+        // В соответствии с заданным интервалом
+        public void Del(DateTime start, DateTime finish)
+        {
+            valuesHolderList.RemoveAll(
+                (Values v) =>
+                {
+                    if (v.Date.CompareTo(start) >= 0 && v.Date.CompareTo(finish) <= 0) return true;
+                    else return false;
+                }
+            );
+        }
+
+        // Функция Sort предназначения для выдачи списка
+        // Отсортированного в соответствии с заданным интервалом
+        public IEnumerable<Values> Sort(DateTime start, DateTime finish)
+        {
+            var query = from val in valuesHolderList
+                        where val.Date.CompareTo(start) >= 0 && val.Date.CompareTo(finish) <= 0
+                        select val;
+            return query;
+        }
+    }
+}
